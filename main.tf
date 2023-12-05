2e739f5cd7e5b84edba8ab9f2da676aefa566473
ovaga <65456274+ovaga@users.noreply.github.com>
2023-12-05 23:38:32 +0100

    VPC Configure (#3)

--- main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "app_server" {
-  ami           = "ami-0230bd60aa48260c6"
-  instance_type = "t3.nano"
-
-  tags = {
-    Name  = "ExampleAppServerInstance"
-    Owner = ""
-  }
-}
